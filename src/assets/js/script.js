a6e24d43166b5770005b47e4c5b3bf871449956e
Salman-3031 <salman5223031@gmail.com>
2025-08-05 22:18:01 +0500

    first commit

---
+++ src/assets/js/script.js
@@ -0,0 +1,46 @@
+// Element references
+const menuToggle = document.getElementById('menuToggle');
+const mobileMenu = document.getElementById('mobileMenu');
+const mobileLinks = document.querySelectorAll('.mobile-links');
+
+function openMenu() {
+  mobileMenu.classList.remove('top-[-500px]');
+  mobileMenu.classList.add('top-[72px]');
+}
+
+function closeMenu() {
+  mobileMenu.classList.remove('top-[72px]');
+  mobileMenu.classList.add('top-[-500px]');
+}
+
+function isMenuOpen() {
+  return mobileMenu.classList.contains('top-[72px]');
+}
+
+// Toggle on button click
+menuToggle.addEventListener('click', () => {
+  isMenuOpen() ? closeMenu() : openMenu();
+});
+
+// Close on outside click
+document.addEventListener('click', (e) => {
+  if (!menuToggle.contains(e.target) && !mobileMenu.contains(e.target)) {
+    closeMenu();
+  }
+});
+
+// Close on link click
+mobileLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    closeMenu();
+  });
+});
+
+
+
+const switchColors = () =>{
+  // document.documentElement.classList.toggle('dark');
+};
+
+
+
